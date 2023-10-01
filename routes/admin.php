7a47c1ab462101a88f6ed229dbbec0da84c5a7c9
Baraaabudraz <abudrazbaraa@gmail.com>
2023-10-02 02:43:54 +0300

    add routes advertisements to route admin

--- routes/admin.php
+++ routes/admin.php
@@ -1,13 +1,16 @@
 <?php
 
+use App\Http\Controllers\Advertisement\PriceController;
+use App\Http\Controllers\Advertisement\ProductAdvertisementController;
+use App\Http\Controllers\Advertisement\StoreAdvertisementController;
 use App\Http\Controllers\Chat\ConversationController;
 use App\Http\Controllers\Store\CategoryController;
 use App\Http\Controllers\Store\FaqsController;
+use App\Http\Controllers\Store\ProductController;
 use App\Http\Controllers\Store\RegionController;
 use App\Http\Controllers\Store\StoreController;
 use App\Http\Controllers\Users\AdminController;
 use App\Http\Controllers\Users\RoleController;
-use App\Models\Chat\Conversation;
 use Illuminate\Support\Facades\Route;
 
 
@@ -25,6 +28,13 @@ Route::prefix('/administrator')->name('admin.')->middleware(['auth:admin'])->gro
     Route::resource('permissions',RegionController::class);
     Route::resource('faqs',FaqsController::class);
     Route::resource('admins',AdminController::class);
+    Route::resource('advertisements',StoreAdvertisementController::class);
+    Route::resource('product-advertisements',ProductAdvertisementController::class);
+    Route::get('advertisements-orders',[StoreAdvertisementController::class,'indexOrder'])->name('advertisements-orders');
+    Route::get('product-advertisements-orders',[ProductAdvertisementController::class,'indexOrder'])->name('product-advertisements-orders');
+    Route::post('store-advertisements-accept/{id}',[StoreAdvertisementController::class,'orderAccepted'])->name('advertisement-store.accept');
+    Route::post('product-advertisements-accept/{id}',[ProductAdvertisementController::class,'orderAccepted'])->name('product-advertisement.accept');
+    Route::resource('prices',PriceController::class);
     Route::get('chat',[ConversationController::class,'index'])->name('chat.index');
     Route::post('chat/search',[ConversationController::class,'search'])->name('chat.search');
     Route::get('chat/show/{id}',[ConversationController::class,'show'])->name('chat.show');
